77cc3916cf97423a40886ff6ac25560a5b4908f6
chenyb <1179506440@qq.com>
2021-08-23 18:00:43 +0800

    增加配置

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,9 @@
 <template>
   <el-button>默认按钮</el-button>
   <el-radio v-model="radio" label="2">备选项</el-radio>
+  <div>
+     111
+  </div>
 </template>
 
 <script>
@@ -12,20 +15,10 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style scoped lang="scss" >
+    $primary: #fff;
+
+    div{
+
+    }
 </style>
